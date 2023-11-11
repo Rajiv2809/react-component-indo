eb4241f8ed59a273895f65ad192a5798ee41b8f7
Rajiv2809 <121161360+Rajiv2809@users.noreply.github.com>
2023-11-11 19:31:39 +0700

    main

---
+++ react-indo/src/utils/data.js
@@ -0,0 +1,26 @@
+
+const getData = () => {
+    return [
+      {
+        id: 1,
+        name: 'Pak Budi ',
+        tag: 'budi_01',
+        imageUrl: 'https://i.pinimg.com/736x/73/65/35/736535cc191c107a9e3a322de1f76950.jpg',
+      },
+      {
+        id: 2,
+        name: 'Bu Sri',
+        tag: 'Sri25_',
+        imageUrl: 'https://i.pinimg.com/564x/b6/4f/79/b64f798bbe07418db6abd047f49c3fa0.jpg',
+      },
+      {
+        id: 3,
+        name: 'rasya',
+        tag: 'raysa',
+        imageUrl: 'https://i.pinimg.com/564x/ed/79/b8/ed79b88276139af8a7bdd9b3c82d8a01.jpg',
+      },
+    ];
+  }
+  
+  export { getData };
+  
